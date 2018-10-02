8757bc744ff472cf38976b6376213f07bfca948b
pedro.lisboa <pedrohblisboa@gmail.com>
2018-10-02 17:36:47 -0300

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="LpsToolbox",
+    version="0.1dev",
+    long_description=open('README.txt').read(),
+    license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007'
+)
