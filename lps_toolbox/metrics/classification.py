ee5f9f388732d459803f512066c8d19459b87721
pedrolisboa <pedrohblisboa@gmail.com>
2018-11-05 11:32:04 -0200

    Added recall_score implementation in tensorflow Updated and created new doctstrings

--- lps_toolbox/metrics/classification.py
+++ lps_toolbox/metrics/classification.py
@@ -1,17 +1,16 @@
 """
-Metrics to acess performance on classification and novelty detection tasks
+Metrics to assess performance on classification and novelty detection tasks
 
-Authors: Pedro Henrique Braga Lisboa <pedrolisboa@poli.ufrj.br>
+Authors: Pedro Henrique Braga Lisboa <pedro.lisboa@lps.ufrj.br>
 """
 
 from __future__ import division
 
 import sklearn
+import numpy as np
 
 from keras import backend as K
-from keras.utils import to_categorical
-
-import numpy as np
+from keras.utils import to_categorical, get_custom_objects
 
 def sp_index(y_true, y_pred):
     """Sum-Product Index Score
@@ -34,10 +33,10 @@ def sp_index(y_true, y_pred):
     if y_true_type != y_pred_type:
         raise TypeError
     elif y_true_type == 'numpy':
-        if y_true.ndim == 1: # turn 1d array to sparse matrix
-            y_true = to_categorical(y_true, np.unique(y_true).shape[0])
         if y_pred.ndim == 1:
             y_pred = to_categorical(y_pred, np.unique(y_true).shape[0])
+        if y_true.ndim == 1:  # turn 1d array to sparse matrix
+            y_true = to_categorical(y_true, np.unique(y_true).shape[0])
 
         sp = _NpMetrics.sp_index(y_true, y_pred)
     elif y_true_type == 'tensorflow':
@@ -48,22 +47,34 @@ def sp_index(y_true, y_pred):
     return sp
 
 
-def recall_score(y_true, y_pred, average=None):
+def recall_score(y_true, y_pred, average=None, clf_idx=None):
     """Sum-Product Index Score
 
-        Parameters
-        ----------
-        y_true : 1d array-like, or label indicator array / sparse matrix / tensorflow sparse tensor
-            Ground truth (correct) labels.
-        y_pred : 1d array-like, or label indicator array / sparse matrix / tensorflow tensor
-            Predicted labels, as returned by a classifier.
+        :param y_true : 1d array-like, or label indicator array / sparse matrix / tensorflow sparse tensor
+                        Ground truth (correct) labels.
+        :param y_pred : 1d array-like, or label indicator array / sparse matrix / tensorflow tensor
+                        Predicted labels, as returned by a classifier.
+        :param average:
 
-        Returns
-        -------
-        recall : float
+        :returns recall : float
     """
+    y_true_type = _check_type(y_true)
+    y_pred_type = _check_type(y_pred)
 
-    recall = sklearn.metrics.recall_score(y_true, y_pred, average=average)
+    if y_true_type != y_pred_type:
+        raise TypeError
+    elif y_true_type == 'numpy':
+        if y_pred.ndim == 1:
+            y_pred = to_categorical(y_pred, np.unique(y_true).shape[0])
+        if y_true.ndim == 1:  # turn 1d array to sparse matrix
+            y_true = to_categorical(y_true, np.unique(y_true).shape[0])
+
+        recall = _NpMetrics.recall_score(y_true, y_pred)
+    elif y_true_type == 'tensorflow':
+        recall = _TfMetrics.recall_score(y_true, y_pred, clf_idx)
+    else:
+        raise TypeError
+
     return recall
 
 
@@ -77,13 +88,12 @@ def f1_score(y_true, y_pred):
 
 def _check_type(array):
     if isinstance(array, np.ndarray):
-        type = 'numpy'
-    elif isinstance(array, K.is_tensor()):
-        type = 'tensorflow'
+        type_obj = 'numpy'
+    elif K.is_tensor(array):
+        type_obj = 'tensorflow'
     else:
-        type = 'unknown'
-
-    return type
+        type_obj = 'unknown'
+    return type_obj
 
 
 class _TfMetrics:
@@ -92,8 +102,14 @@ class _TfMetrics:
 
     @staticmethod
     def sp_index(y_true, y_pred):
+        """
+
+        :param y_true:
+        :param y_pred:
+        :return:
+        """
         num_classes = K.int_shape(y_pred)[1]  # y_true returns (None, None) for int_shape
-        # y_pred returns (None, num_classes)
+                                              # y_pred returns (None, num_classes)
 
         true_positives = K.sum(K.cast(y_true * K.one_hot(K.argmax(y_pred, axis=1), num_classes), dtype='float32'),
                                axis=0)
@@ -104,7 +120,21 @@ class _TfMetrics:
         return sp
 
     @staticmethod
+    def recall_score(y_true, y_pred, cls_idx):
+        true_positives = K.sum(K.cast(y_true * K.one_hot(K.argmax(y_pred, axis=1), num_classes), dtype='float32'),
+                               axis=0)
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)), axis=0)
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall[cls_idx]
+
+    @staticmethod
     def accuracy_score(y_true, y_pred):
+        """
+
+        :param y_true:
+        :param y_pred:
+        :return:
+        """
         num_classes = K.int_shape(y_pred)[1]  # y_true returns (None, None) for int_shape
         # y_pred returns (None, num_classes)
 
@@ -116,21 +146,38 @@ class _TfMetrics:
 
 
 class _NpMetrics:
-    def __int__(self):
+    def __init__(self):
         pass
 
     @staticmethod
     def np_accuracy_score(y_true, y_pred):
+        """
+
+        :param y_true:
+        :param y_pred:
+        :return:
+        """
         num_classes = y_true.shape[1]
         recall = recall_score(y_true, y_pred, None)
         return np.sum(recall) / num_classes
 
     @staticmethod
     def sp_index(y_true, y_pred):
+        """
+
+        :param y_true:
+        :param y_pred:
+        :return:
+        """
         num_classes = y_true.shape[1]
         recall = recall_score(y_true, y_pred, None)
-        sp = np.sqrt(np.sum(recall) / num_classes * \
-                     np.power(np.prod(recall), 1.0 / float(num_classes))
-                     )
+        sp = np.sqrt(np.sum(recall) / num_classes *
+                     np.power(np.prod(recall), 1.0 / float(num_classes)))
         return sp
 
+    @staticmethod
+    def recall_score(y_true, y_pred, average=None):
+        recall = sklearn.metrics.recall_score(y_true, y_pred, average=average)
+        return recall
+
+get_custom_objects().update({"sp_index": sp_index})
--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="LpsToolbox",
     version="0.1dev",
     long_description=open('README.md').read(),
-    license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007'
+    license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007', install_requires=['scikit-learn', 'pandas', 'keras',
+                                                                                    'seaborn', 'joblib']
 )
