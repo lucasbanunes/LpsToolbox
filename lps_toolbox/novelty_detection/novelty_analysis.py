0eea1c33bf56641e30b1fb344902c00fc19dcdb2
lucasbanunes <lucasbanunes@gmail.com>
2020-02-28 17:44:19 -0300

    The frames does not support string names anymore all the classes must be an int. Editted comments.

--- lps_toolbox/novelty_detection/novelty_analysis.py
+++ lps_toolbox/novelty_detection/novelty_analysis.py
@@ -30,11 +30,10 @@ def create_threshold(quantity,
         return threshold
 
 def get_results(predictions,
-                labels,
-                classes_names, 
-                novelty_index,
-                threshold, 
-                novelty_matrix = False,
+                target,
+                real_cls_values, 
+                novelty_class,
+                threshold,
                 save_csv = False, 
                 filepath = './',
                 filename = None):
@@ -47,14 +46,15 @@ def get_results(predictions,
         predictions: numpy array
             prediction set with events on lines ans neurons on columns
 
-        labels: numpy array
-            correct labels of the prediction set
+        target: numpy array
+            correct target of the prediction set (can't be one hot encoded)
+            Example: target = [1,0,0,2,3, ..., 3]
 
-        classes_names: list
-            list which the position i has the name of the class i
+        real_cls_values: 1-D array like
+            list which the position i has the correct number of the class i in the network
 
-        novelty_index: int
-            index of the class that will be treated as novelty
+        novelty_class: int
+            class that will be treated as novelty
             
         threshold: numpy array
             array with one or multiples values of threshold to be tested    
@@ -70,8 +70,8 @@ def get_results(predictions,
             novelty_frame: pandas.DataFrame
         """
 
-        classf = _get_classf_matrix(predictions, classes_names, novelty_index, threshold)
-        target = _get_target_matrix(labels, classes_names, novelty_index)
+        classf = _get_classf_matrix(predictions, real_cls_values, novelty_class, threshold)
+        target = target.reshape(-1,1)
         data = np.concatenate((predictions, classf, target), axis = 1)
         outer_level = ['Neurons', 'Neurons', 'Neurons', 'Target']
         inner_level = ['out0', 'out1', 'out2', 'T']
@@ -80,14 +80,13 @@ def get_results(predictions,
             inner_level.insert(-1, t)
         novelty_frame = pd.DataFrame(data, columns = pd.MultiIndex.from_arrays([outer_level, inner_level]))
         if save_csv:
-            #filepath = filepath +'/threshold_from_' + str(threshold[0]) + 'to' + str(threshold[-1]) + '.csv'
             filepath = filepath + '/' +filename
             novelty_frame.to_csv(filepath, index = False)
         return novelty_frame
 
 def read_results_csv(filepath):
     """
-    reads the csv file and returns the data frame
+    reads the csv file generated from get_results() and returns the DataFrame
 
     Parameters
 
@@ -120,7 +119,10 @@ def get_novelty_eff(results_frame,
     Parameters
 
     results_frame: pandas.DataFrame
-        frame obtained from get_results
+        frame obtained from get_results function
+
+    novelty_class
+        class treated as novelty
     
     average: None or string
         parameter defined for sklearn.metrics.precision_recall_fscore_support
@@ -137,17 +139,16 @@ def get_novelty_eff(results_frame,
     Return
         eff_frame: pandas.DataFrame
     """
-    y_true = np.where(results_frame['Target']['T'].values == novelty_class, 1, 0)
+    y_true = np.where(results_frame.loc[:, 'Target'].values == novelty_class, 1, 0)
     novelty_matrix = np.array(get_novelty_from_frame(results_frame, novelty_class), dtype = float)
-    thresh = results_frame['Classification'].columns.values
+    thresh = results_frame.loc[:, 'Classification'].columns.values
     precision = _get_precision(y_true, novelty_matrix, average=average)
     recall = _get_recall(y_true, novelty_matrix, average=average)
     fbeta = _get_fbeta(y_true, novelty_matrix, beta, average=average)
-    inner_level = np.concatenate((thresh, thresh))
     if average:
         sp = _get_sp_index(y_true, novelty_matrix)
         acc = _get_accuracy(y_true, novelty_matrix, normalize)
-        novelty_rate = _get_novelty_rate(y_true, novelty_matrix)
+        novelty_rate = class_detection_rate(y_true, novelty_matrix)
         if verbose:
             print(f'Precision array shape is {precision.shape} and it is:\n{precision}')
             print(f'Recall array shape is {recall.shape} and it is:\n{recall}')
@@ -156,19 +157,17 @@ def get_novelty_eff(results_frame,
             print(f'SP array shape is {sp.shape} and it is:\n{sp}')
         data = np.concatenate((acc.reshape(-1,1), precision.reshape(-1,1), recall.reshape(-1,1), fbeta.reshape(-1,1), sp.reshape(-1,1), novelty_rate.reshape(-1,1)), axis=1)
         nov_eff_frame = pd.DataFrame(data, index = thresh, columns = ['Accuracy', 'Precision', 'Recall', 'Fbeta', 'SP', 'Novelty Rate'])
+        nov_eff_frame.index.name = 'Threshold'
     else:
         if verbose:
+            print(f'y_true array shape is {y_true.shape} and it is:\n{y_true}')
+            print(f'novelty_matrix.shape is {novelty_matrix.shape} and it is:\n{novelty_matrix}')
             print(f'Precision array shape is {precision.shape} and it is:\n{precision}')
             print(f'Recall array shape is {recall.shape} and it is:\n{recall}')
             print(f'Fbeta array shape is {fbeta.shape} and it is:\n{fbeta}')
         data = np.concatenate((precision[0].reshape(-1,1), recall[0].reshape(-1,1), fbeta[0].reshape(-1,1)), axis=1)
         data = np.concatenate((data, np.concatenate((precision[1].reshape(-1,1), recall[1].reshape(-1,1), fbeta[1].reshape(-1,1)), axis=1)), axis = 0)
-        outer_level = list()
-        inner_level = np.concatenate((thresh, thresh))
-        for i in range(2):
-            for j in range(len(thresh)):
-                outer_level.append(i)
-        nov_eff_frame = pd.DataFrame(data, index = pd.MultiIndex.from_arrays([outer_level, inner_level]), columns = ['Precision', 'Recall', 'Fbeta'])
+        nov_eff_frame = pd.DataFrame(data, index = pd.MultiIndex.from_product([[0,1], thresh], names=('Novelty', 'Threshold')), columns = ['Precision', 'Recall', 'Fbeta'])
     if save_csv:
         filepath = filepath + '/' + filename
         nov_eff_frame.to_csv(filepath, index = True)
@@ -190,6 +189,9 @@ def get_classf_eff(results_frame,
 
     results_frame: pandas.DataFrame
         frame obtained from get_results
+
+    labels: 1-d array like
+        contains the name of the classes or its numbers
     
     average: None or string
         parameter defined for sklearn.metrics.precision_recall_fscore_support
@@ -218,55 +220,24 @@ def get_classf_eff(results_frame,
         print(f'The precision array has shape {fbeta.shape} and it is:\n{fbeta}')
         print('Generating data frame')
     if average:
-        sp = np.apply_along_axis(lambda x: sp_index_multiclass(y_true, x), 0, classf_matrix)
+        sp = _get_sp_index(y_true, classf_matrix)
         acc = np.apply_along_axis(lambda x: accuracy_score(y_true, x, normalize), 0, classf_matrix)
         data = np.concatenate((acc.reshape(-1,1), precision.reshape(-1,1), recall.reshape(-1,1), fbeta.reshape(-1,1), sp.reshape(-1,1)), axis = 1)
-        eff_frame = pd.DataFrame(data, index = thresh, columns = ['Accuracy', 'Precision', 'Recall', 'Fbeta', 'SP multiclass'])
+        eff_frame = pd.DataFrame(data, index = thresh, columns = ['Accuracy', 'Precision', 'Recall', 'Fbeta', 'SP'])
+        eff_frame.index.name = 'Threshold'
     else:
-        len_thresh = len(thresh)
-        for p, r, f, s, c in zip(precision, recall, fbeta, sp_binary, classes_names):
+        for p, r, f, sp, c in zip(precision, recall, fbeta, sp_binary, classes_names):
             if data_creation:
-                data = np.concatenate((p.reshape(-1,1), r.reshape(-1,1), f.reshape(-1,1), s.reshape(-1,1)), axis = 1)
-                inner_level = thresh
-                outer_level = list(c)
-                for i in range(len_thresh -1):
-                    outer_level.append(c)
+                data = np.concatenate((p.reshape(-1,1), r.reshape(-1,1), f.reshape(-1,1), sp.reshape(-1,1)), axis = 1)
                 data_creation = False
             else:
-                d = np.concatenate((p.reshape(-1,1), r.reshape(-1,1), f.reshape(-1,1), s.reshape(-1,1)), axis = 1)
+                d = np.concatenate((p.reshape(-1,1), r.reshape(-1,1), f.reshape(-1,1), sp.reshape(-1,1)), axis = 1)
                 data = np.concatenate((data, d), axis = 0)
-                inner_level = np.append(inner_level, thresh)
-                for i in range(len_thresh):
-                    outer_level.append(c)
-        if verbose:
-            print(f'The data array has shape {data.shape} and it is:\n{data}')
-            print(f'The outer level is: {outer_level}')
-            print(f'The inner level is: {inner_level}')
-        eff_frame = pd.DataFrame(data, index = pd.MultiIndex.from_arrays([outer_level, inner_level]), columns = ['Precision', 'Recall', 'Fbeta', 'Binary SP Index'])
+        eff_frame = pd.DataFrame(data, index = pd.MultiIndex.from_product([np.unique(y_true), thresh], names=('Class', 'Threshold')), columns = ['Precision', 'Recall', 'Fbeta', 'Binary SP Index'])
     if save_csv:
         filepath = filepath + '/' +filename
         eff_frame.to_csv(filepath, index = True)
-    return eff_frame
-
-
-def get_confusion_matrix(results_frame, classes_names, labels = None, save_csv = False, filepath = './', filename = None):
-    y_true = results_frame.loc[:,'Target'].values
-    pred_matrix = results_frame.loc[:,'Classification'].values
-    thresh = results_frame.loc[:,'Classification'].columns
-    matrices = list()
-    outer_level = list()
-    inner_level = list()
-    for y_pred, t in zip(pred_matrix.T, thresh):
-        matrices.append(confusion_matrix(y_true, y_pred, labels = classes_names))
-        for c in classes_names:
-            outer_level.append(t)
-            inner_level.append(c)
-    confusion_frame = pd.DataFrame(np.array(matrices).reshape(len(thresh)*4, 4), index = pd.MultiIndex.from_arrays([outer_level, inner_level]), columns = classes_names)
-    if save_csv:
-        filepath = filepath + '/' + filename
-        confusion_frame.to_csv(filepath, index = True)
-    return confusion_frame
-    
+    return eff_frame    
 
 def get_novelty_from_frame(results_frame, novelty_class):
     """
@@ -280,7 +251,7 @@ def get_novelty_from_frame(results_frame, novelty_class):
     Return
         novelty_matrix: numpy.ndarray
     """
-    return np.where(results_frame['Classification'].values == novelty_class, True, False)
+    return np.where(results_frame.loc[:, 'Classification'].values == novelty_class, True, False)
 
 def _get_label_matrix(results_frame,
                       classes_names, 
@@ -324,7 +295,7 @@ def _get_novelty_matrix(predictions, threshold):
             array with the threshold values
         
         Return
-            novelty_matrix: numpy.ndarray
+            novelty_matrix: numpy.ndarray, dtype = bool
         """
 
         novelty_matrix = np.empty(shape = (predictions.shape[0], threshold.shape[0]), dtype = bool)
@@ -334,70 +305,63 @@ def _get_novelty_matrix(predictions, threshold):
         return novelty_matrix
             
 def _get_classf_matrix(predictions, 
-                       classes_names,
-                       novelty_index, 
+                       real_cls_values,
+                       novelty_class, 
                        threshold):
         """
-        creates an array with the classfication for the events as rows and foe each value of the threshold 
-        as a column
+        creates an array with the classfication for the events as rows and for each value of the threshold as a column
 
         Parameters
 
         predictions: numpy array
-            prediction set
-        classes_names: list
-            list which the position i has the name of the class i
-         novelty_index: int
-            index of the class that will be treated as novelty
+            prediction matrix
+        real_cls_values: 1-D array like
+            list which the position i has the correct number of the class i in the network
+        novelty_class: int
+            class that was treated as novelty
         threshold: numpy array
             array with the threshold values
         
         Return
-            classf_matrix: numpy.ndarray
+            classf_matrix: numpy.ndarray, dtype=object
         """
 
-        novelty = classes_names[novelty_index]
-        classes_names.pop(novelty_index)
         novelty_matrix = _get_novelty_matrix(predictions, threshold)
         stack = list()
         results = list()
         for i in np.argmax(predictions, axis = 1):
-            #print(classes_names)
-            #print(i)
-            results.append(classes_names[int(i)])
-        classes_names.insert(novelty_index, novelty)
+            results.append(real_cls_values[int(i)])
         for i in range(threshold.shape[0]):
             stack.append(results)
-        classf_matrix = np.where(novelty_matrix, novelty, np.column_stack(tuple(stack)))
+        classf_matrix = np.where(novelty_matrix, novelty_class, np.column_stack(tuple(stack)))
         return classf_matrix
 
         
     
-def _get_target_matrix(labels, 
+def _get_target_matrix(target, 
                        classes_names, 
                        novelty_index):
         """
         creates an array with the target for the events as rows
+        uses the classes names
 
         Parameters
 
-        labels: numpy array
-            correct labels of the prediction set
-        classes_names: list
-            list which the position i has the name of the class i
+        target: numpy array
+            correct target of the prediction set
+        classes_names: 1-D array like
+            list which the position i has the correct number of the class i in the network
         novelty_index: int
             index of the class that will be treated as novelty
 
         Return
-            target_matrix: numpy.ndarray
+            target_matrix: numpy.ndarray, dtype=object
         """
-        #novelty = classes_names[novelty_index]
-        #classes_names[novelty_index] = 'Nov'
-        target = list()
-        for l in labels:
-            target.append(classes_names[int(l)])
-        #classes_names[novelty_index] = novelty
-        target_matrix = np.array(target).reshape((-1, 1))
+
+        named_target = list()
+        for t in target:
+            named_target.append(classes_names[int(t)])
+        target_matrix = np.array(named_target).reshape((-1, 1))
         return target_matrix
     
 def _get_accuracy(y_true, pred_matrix, normalize=True, sample_weight=None):
@@ -412,7 +376,17 @@ def _get_sp_index(y_true, pred_matrix):
     implementation of lps_toolbox.metrics.classification,sp_index for the novelty matrix
     the parameters work as the other function
     """
-    return np.apply_along_axis(lambda x: sp_index(y_true, x), 0, pred_matrix)
+    y_true = y_true.flatten()
+    if len(pred_matrix.shape) == 2:
+        sp = np.array([])
+        for prediction in pred_matrix.T:
+            sp = np.append(sp, sp_index(y_true, prediction))
+        
+        return sp
+    elif (len(pred_matrix.shape) == 1) or (pred_matrix.shape[1] == 1):
+        return(y_true, pred_matrix.flatten())
+    else:
+        raise ValueError('The prediction matrix must be 2d or 1d')
 
 def _get_recall(y_true, pred_matrix, labels=None, pos_label=1, average='binary', sample_weight=None):
     """
@@ -440,14 +414,35 @@ def _get_fbeta(y_true, pred_matrix, beta, labels=None, pos_label=1, average='bin
 
 def _get_sp_binary(y_true, pred_matrix, classes):
     sp = list()
-    for cls in classes:
-        sp.append(np.apply_along_axis(lambda x: sp_index_binary(y_true, x, cls), 0, pred_matrix))
+    for clas in classes:
+        sp.append(np.apply_along_axis(lambda x: sp_index_binary(y_true, x, clas), 0, pred_matrix))
     return np.array(sp)
 
-def _get_novelty_rate(y_true, pred_matrix):
-    novelty_rate = list()
-    novelties = np.sum(y_true == 1)
-    for column in range(pred_matrix.shape[1]):
-        pred_novelties = np.sum(pred_matrix[:,column] == 1)
-        novelty_rate.append((pred_novelties/novelties))
-    return np.array(novelty_rate)
+def class_detection_rate(y_true, pred_matrix, clas = None):
+    """
+    detection ratio of a determined class in comparison with the correct number of occurrences
+    Parameters:
+
+    y_true: numpy array
+        Targeted classification if no "clas" parameter is provided the desired class to be calculated must be 1 and the others 0
+        
+    pred_matrix: numpy.array
+        Classfication obtained from the model (events as rows) if no "clas" parameter is provided the desired class to be calculated must be 1 and the others 0
+        
+
+    clas: int
+        Clas that will have its detection rate calculated
+    
+    Returns:
+    detection_rate: int or 1d numpy array
+    """
+    if not (type(clas) == type(None)):
+        y_true = np.where(y_true == clas, 1, 0)
+        pred_matrix = np.where(pred_matrix == clas, 1, 0)
+
+    detection_rate = np.array([])
+    occurrences = np.sum(y_true == 1)
+    for column in pred_matrix.T:
+        pred_occurences = np.sum(column == 1)
+        detection_rate = np.append(detection_rate, (pred_occurences/occurrences))
+    return detection_rate
