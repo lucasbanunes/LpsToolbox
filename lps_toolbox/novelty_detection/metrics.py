1e41bff44a213942c16a8175ce27cad6ceae8ed6
lucasbanunes <lucasbanunes@gmail.com>
2020-03-07 18:06:09 -0300

    Removed sp_index_multiclass and add sp_index.

--- lps_toolbox/novelty_detection/metrics.py
+++ lps_toolbox/novelty_detection/metrics.py
@@ -3,13 +3,6 @@ Author: Lucas Barra de Aguiar Nunes"""
 import numpy as np
 from sklearn.metrics import recall_score
 
-def sp_index_multiclass(y_true, y_pred, labels = None):
-    n_classes = len(np.unique(y_true))
-    exponent = 1/n_classes
-    recall = recall_score(y_true, y_pred, labels = labels, average = None)
-    sp = np.sqrt((np.sum(recall)/n_classes)*(np.power(np.prod(recall), exponent)))
-    return sp
-
 def sp_index_binary(y_true, y_pred, correct_cls, labels = None):
     y_true = np.where(y_true == correct_cls, 1, 0)
     y_pred = np.where(y_pred == correct_cls, 1, 0)
@@ -17,4 +10,11 @@ def sp_index_binary(y_true, y_pred, correct_cls, labels = None):
     exponent = 1/n_classes
     recall = recall_score(y_true, y_pred, labels = labels, average = None)
     sp = np.sqrt((np.sum(recall)/n_classes)*(np.power(np.prod(recall), exponent)))
+    return sp
+
+def sp_index(y_true, y_pred):
+    labels = np.unique(y_true)
+    num_classes = len(labels)
+    recall = recall_score(y_true, y_pred, labels=labels, average=None)
+    sp = np.sqrt((np.sum(recall)/num_classes)*(np.power(np.prod(recall), (1/num_classes))))
     return sp
